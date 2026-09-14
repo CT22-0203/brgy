ad056a0209f9dd85937031ebdd36537776971937
CT22-0203 <karlvincent.puno@yahoo.com>
2026-09-14 19:28:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+const form = document.querySelector(".form-index");
+
+const nameInput = document.getElementById("f-name");
+nameInput.addEventListener("input", function () {
+    this.value = this.value.replace(/[^a-zA-ZñÑ\s.]/g,"");
+});
+
+["chairs", "tables", "tents"].forEach(function (id) {
+    const input = document.getElementById(id);
+
+    input.addEventListener("keydown", function (event) {
+        const blockedKeys = ["e", "E", "+", "-", "."];
+        if (blockedKeys.includes(event.key)) {
+            event.preventDefault();
+        }
+    });
+
+
+    input.addEventListener("input", function () {
+        this.value = this.value.replace(/[^0-9]/g, "");
+    });
+});
+
+
+form.addEventListener("submit", function (event) {
+
+    const name = document.getElementById("f-name").value;
+    const address = document.getElementById("address").value;
+
+    const chairs = document.getElementById("chairs").value;
+    const tables = document.getElementById("tables").value;
+    const tents = document.getElementById("tents").value;
+
+    const dateBorrowed = document.getElementById("dateBorrowed").value;
+    const dateReturned = document.getElementById("dateReturned").value;
+
+    const validIDFile = document.getElementById("validID").files[0];
+
+
+    if (name.trim() === " ") {
+        event.preventDefault();
+        alert("Please fill in all required fields.");
+        return;
+    }
+
+    if (address.trim() === " ") {
+        event.preventDefault();
+        alert("Please fill in all required fields.");
+        return;
+    }
+
+    if (Number(chairs) === 0) {
+        event.preventDefault();
+        alert("Please select at least a minimum number of Chairs.");
+        return;
+    }
+
+    if (Number(tables) === 0) {
+        event.preventDefault();
+        alert("Please select at least a minimum number of Tables.");
+        return;
+    }
+
+    if (Number(tents) === 0) {
+        event.preventDefault();
+        alert("Please select at least a minimum number of Tents.");
+        return;
+    }
+    if (!validIDFile) {
+        event.preventDefault();
+        alert("Please upload a valid ID with signature.");
+        return;
+    }
+
+    if (new Date(dateReturned) <= new Date(dateBorrowed)) {
+        event.preventDefault();
+        alert("The return date must be later than the borrowed date.");
+        return;
+    }
+
+    if (agreeCheckbox.checked === false) {
+        event.preventDefault();
+        alert("Please agree to the terms and conditions.");
+        return;
+    }
+
+    const confirmSubmit = confirm(
+        "Are you sure you want to submit your borrower slip?"
+    );
+
+    if (!confirmSubmit) {
+        event.preventDefault();
+        return;
+
+    }
+    alert("Your borrower slip has been submitted successfully.");
+
+    const borrowerData = {
+        name: name,
+        address: address,
+        chairs: chairs,
+        tables: tables,
+        tents: tents,
+        dateBorrowed: dateBorrowed,
+        dateReturned: dateReturned,
+        validIDFileName: validIDFile.name
+    };
+
+    localStorage.setItem(
+        "borrowerData",
+        JSON.stringify(borrowerData)
+    );
+
+});
